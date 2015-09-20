2fd5e2f9e0fdc42fe4ca3aa147dab698ba49e818
LaazyJ <github@jasonduffett.org>
2015-09-20 16:10:46 +0100

    Service1 Micro-Service
    The first of the Micro-Services in our system.

---
+++ MessageBusArchitectureExample.Service1/Container.cs
@@ -0,0 +1,19 @@
+﻿using Castle.Windsor;
+using Castle.Windsor.Installer;
+using NServiceBus;
+
+namespace MessageBusArchitectureExample.Service1
+{
+    /// <summary>
+    /// Configure Castle Windsor as the IoC container in this solution.
+    /// </summary>
+    public class Container : INeedInitialization
+    {
+        public static readonly IWindsorContainer Instance = new WindsorContainer();
+
+        public void Customize(BusConfiguration configuration)
+        {
+            Instance.Install(FromAssembly.This());
+        }
+    }
+}
---
+++ MessageBusArchitectureExample.Service1/FireAndForgetCommandProcessor.cs
@@ -0,0 +1,32 @@
+﻿using MessageBusArchitectureExample.Service1.Messages.Commands;
+using MessageBusArchitectureExample.Service1.Messages.Events;
+using NLog;
+using NServiceBus;
+
+namespace MessageBusArchitectureExample.Service1
+{
+    /// <summary>
+    /// Message Handler for FireAndForgetCommands.
+    /// For demonstration purposes it just logs that the message was received and publishes an IFireAndForgetCommandReceivedEvent.
+    /// </summary>
+    public class FireAndForgetCommandProcessor :
+        IHandleMessages<FireAndForgetCommand>
+    {
+        public static ILogger Log = LogManager.GetCurrentClassLogger();
+
+        public IBus Bus { get; set; }
+
+        public void Handle(FireAndForgetCommand message)
+        {
+            Log.Info("FireAndForgetCommand received for Entity '{0}'", message.EntityId);
+
+            // Do some work here
+
+            // Work is done, let interested parties know.
+            Bus.Publish<IFireAndForgetCommandReceivedEvent>(evt =>
+            {
+                evt.EntityId = message.EntityId;
+            });
+        }
+    }
+}
